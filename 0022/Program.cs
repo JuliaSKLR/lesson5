6a001266ee6890a71d7cb475d03b0a46ed287a90
Julia.S <der.lavender@gmail.com>
2022-08-24 19:10:20 +0300

    013

---
+++ 0022/Program.cs
@@ -0,0 +1,36 @@
+﻿//Напишите программу замены элементов массива: положительные элементы
+// замените на соответствующие отрицательные, и наоборот.
+//[-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+int[] CreateArray(int length, int min, int max)
+{
+    int[] answer = new int[length];
+    Random rnd = new Random();
+    for (int i = 0; i < answer.Length; i++)
+    {
+        answer[i] = rnd.Next(min, max + 1);
+    }
+    return answer;
+}
+
+void PrintArray(int[] array) // вывод массива на консоль
+{
+    for (int j = 0; j < array.Length; j++)
+    {
+        Console.Write(array[j] + "\t");
+    }
+    System.Console.WriteLine();
+}
+
+int[] Perevorot(int[] a)
+{
+    int count = a.Length;
+    for (int i = 0; i < count; i++)
+    {
+        a[i] = -a[i];
+    }
+    return a;
+}
+int[] array1 = CreateArray(8, -40, 40);
+PrintArray(array1);
+PrintArray(Perevorot(array1));
